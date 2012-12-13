12ae51265f0d39d7d6ac5e0d04a3cdc5ea226963
bd <bdletterbox@gmail.com>
2012-12-13 16:11:23 +0400

    Added delate, erode, threshold to detect objects

--- trunk/src/program.py
+++ trunk/src/program.py
@@ -15,7 +15,7 @@ HORIZONTAL_FLIP = 1 # Const horisontal flipping mode
 # Параметр, устойчивости фона
 # 0 <= alpha <= 1
 # Чем меньше, тем фон менее устойчив 
-alpha = 0.9
+alpha = 0.94
 
 capture = cv.CreateCameraCapture(0)
 
@@ -41,11 +41,13 @@ while(True):
     
     # Set first background
     if(background == None):
+        
         background = cv.CreateImage((sourceFrame.width, sourceFrame.height), sourceFrame.depth, sourceFrame.nChannels)
+        
         # Horizontal flipping of the image
         cv.Flip(sourceFrame, background, HORIZONTAL_FLIP)
-        
         cv.Smooth(background, background, cv.CV_GAUSSIAN)
+        
     
     frame = cv.CreateImage((sourceFrame.width, sourceFrame.height), sourceFrame.depth, sourceFrame.nChannels)
     # Horizontal flipping of the image
@@ -55,41 +57,50 @@ while(True):
     
     # Создает новое изображение, с такими же параметрами, как у всех изображений в видео 
     newBackground = cv.CreateImage((frame.width, frame.height), frame.depth, frame.nChannels)
+    subCurrentFrame = cv.CreateImage((frame.width, frame.height), frame.depth, frame.nChannels)
+    cv.Sub(frame, background, subCurrentFrame)
     cv.AddWeighted(background, alpha, frame, (1 - alpha), 0, newBackground)
     
+    """
     # Show frame
     cv.ShowImage(sourceWindow, frame)
     cv.ShowImage(backgroundWindow, newBackground)
+    """
     
     grayFrame = cv.CreateImage(cv.GetSize(frame), frame.depth, 1)
     cv.CvtColor(frame, grayFrame, cv.CV_RGB2GRAY)
     
     grayBack = cv.CreateImage(cv.GetSize(frame), frame.depth, 1)
-    cv.CvtColor(newBackground, grayBack, cv.CV_RGB2GRAY)
+    cv.CvtColor(background, grayBack, cv.CV_RGB2GRAY)
+
+    cv.ShowImage(sourceWindow, grayFrame)
+    cv.ShowImage(backgroundWindow, grayBack)
     
     print "debug"
     
+    # Sub for gray images
     sub = cv.CreateImage(cv.GetSize(frame), frame.depth, 1)
     cv.AbsDiff(grayFrame, grayBack, sub)
-    
-    cv.ShowImage(subWindow, sub)
+    #cv.AbsDiffS(sub, sub, 0.5)
     
     """
     sub = cv.CreateImage(cv.GetSize(frame), frame.depth, frame.nChannels)
     cv.AbsDiff(frame, background, sub)
+    cv.AbsDiffS(sub, sub, 100)
     """
     
     # threshold(foreground,foreground,128,255,THRESH_BINARY);
-    cv.Threshold(sub, sub, 128, 255, cv.CV_THRESH_BINARY)
+    #cv.AdaptiveThreshold(sub, sub, 255, cv.CV_ADAPTIVE_THRESH_GAUSSIAN_C, cv.CV_THRESH_BINARY, 9)
+    cv.Threshold(sub, sub, 15, 255, cv.CV_THRESH_BINARY)
     
     #medianBlur(foreground,foreground,9);
     cv.Smooth(sub, sub, cv.CV_BLUR)
     #erode(foreground,foreground,Mat());
-    cv.Erode(sub, sub)
+    cv.Erode(sub, sub, None, 3)
     #dilate(foreground,foreground,Mat());
-    cv.Dilate(sub, sub)
+    cv.Dilate(sub, sub, None, 3)
 
-    #cv.ShowImage(subWindow, sub)
+    cv.ShowImage(subWindow, sub)
 
     # Обязательно нужна функция задержки!
     # Иначе видео пробегается моментально и на экране ничего не отображается!
